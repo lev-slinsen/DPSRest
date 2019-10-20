12aafec5f67d38ad29da01cf1442591b7d198e2f
Ice Ivanov <ice.ivanov@gmail.com>
2019-10-20 21:47:07 +0300

    added fields to api view

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,8 +1,23 @@
-from .models import Pizza
+from .models import Pizza, Size, Taste
 from rest_framework import serializers
 
 
+class SizeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Size
+        fields = ['name']
+
+
+class TasteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Taste
+        fields = ['name']
+
+
 class PizzaSerializer(serializers.HyperlinkedModelSerializer):
+    size = SizeSerializer(read_only=True, many=True)
+    taste = TasteSerializer(read_only=True, many=True)
+
     class Meta:
         model = Pizza
-        fields = ['name', 'price']
+        fields = ['name', 'price', 'text_short', 'text_long', 'size', 'taste', 'photo']
