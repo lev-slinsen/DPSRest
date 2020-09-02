4454274da1635ae4f530e9f525c729770fb78473
Ice Ivanov <ice.ivanov@gmail.com>
2020-09-03 00:23:47 +0300

    Updates

---
+++ precache-manifest.14c378e5830e28452350013bcf4ee30a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24f06cdc2d69ffbb793679e28c8c2a5",
+    "url": "/DPSRest/index.html"
+  },
+  {
+    "revision": "adbfc0c2b2614e1da32f",
+    "url": "/DPSRest/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "874d2d3352f93ef53a6f",
+    "url": "/DPSRest/static/css/3.8c234dc9.chunk.css"
+  },
+  {
+    "revision": "f988931a89f9d083e6d1",
+    "url": "/DPSRest/static/css/main.466df450.chunk.css"
+  },
+  {
+    "revision": "adbfc0c2b2614e1da32f",
+    "url": "/DPSRest/static/js/2.7ecda528.chunk.js"
+  },
+  {
+    "revision": "99d31cac8f650aa1f1c9574559942819",
+    "url": "/DPSRest/static/js/2.7ecda528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874d2d3352f93ef53a6f",
+    "url": "/DPSRest/static/js/3.c1f1dc64.chunk.js"
+  },
+  {
+    "revision": "f988931a89f9d083e6d1",
+    "url": "/DPSRest/static/js/main.018377af.chunk.js"
+  },
+  {
+    "revision": "047c9bc38682615282b2",
+    "url": "/DPSRest/static/js/runtime-main.631f8b34.js"
+  },
+  {
+    "revision": "3e6608c53299d6bb5ba946533fc880f7",
+    "url": "/DPSRest/static/media/Spinner.3e6608c5.svg"
+  },
+  {
+    "revision": "fa681005ce7695d44d7112381f2f59e0",
+    "url": "/DPSRest/static/media/logo.fa681005.svg"
+  },
+  {
+    "revision": "6a8d950c1de3a92f5d243168fb52fe03",
+    "url": "/DPSRest/static/media/sets.6a8d950c.png"
+  },
+  {
+    "revision": "e4025e5ca1eace59a2080cd91342d5cd",
+    "url": "/DPSRest/static/media/single.e4025e5c.png"
+  },
+  {
+    "revision": "26088e75ebc10fb3b4eb5caeb74bac34",
+    "url": "/DPSRest/static/media/slide1.26088e75.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b989328862342f2aedcccdaa37c2d429.js"
+  "/DPSRest/precache-manifest.14c378e5830e28452350013bcf4ee30a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DPSRest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
